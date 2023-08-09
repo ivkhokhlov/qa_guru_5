fa99afc3f6cb4e23101d44f74fba99d663a3f2d7
Ivan Khokhlov <xxx.mailbox73@gmail.com>
2023-08-09 13:49:32 +0400

    HW-5-2

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,10 @@ from selene import browser
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    browser.config.driver_name = 'firefox'
+    browser.config.driver_name = 'chrome'
     browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_height = 1200
+    browser.config.window_width = 1200
     yield
     browser.quit()
 
--- tests/test_student_registration_form.py
+++ tests/test_student_registration_form.py
@@ -1,34 +1,44 @@
 from selene import browser
 from selene import be, have
 from random import choice
+import os
 
 
-def test_submit_form_with_valid_data(browser_management, faker, tmp_path):
+def test_submit_form_with_valid_data(browser_management, faker):
     browser.open('/automation-practice-form')
 
-    browser.element('#firstName').should(be.blank).type(faker.first_name())
-    browser.element('#lastName').should(be.blank).type(faker.last_name())
-    browser.element('#userEmail').should(be.blank).type(faker.email())
+    browser.element('#firstName').should(be.blank).type('Ivan')
+    browser.element('#lastName').should(be.blank).type('Khokhlov')
+    browser.element('#userEmail').should(be.blank).type('ivan@example.com')
     browser.element('#genterWrapper').element('[name="gender"]').double_click()
-    browser.element('#userNumber').should(be.blank).type(faker.random_number(digits=10))
+    browser.element('#userNumber').should(be.blank).type('0123456789')
+
 
     browser.element('#dateOfBirthInput').click()
-    choice(browser.all('.react-datepicker__day')).click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('.react-datepicker__month-select [value="8"]').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('.react-datepicker__year-select [value="1993"]').click()
+    browser.element('.react-datepicker__day--015').click()
 
     browser.element('#subjectsInput').should(be.blank).type('Maths').press_enter()
     browser.element("[for='hobbies-checkbox-3']").click()
-
-    file = tmp_path / "file.jpg"
-    file.write_bytes(faker.image())
-    browser.element('#uploadPicture').send_keys(str(file))
-
-    browser.element('#currentAddress').should(be.blank).type(faker.address())
+    browser.element('#uploadPicture').send_keys(os.path.abspath('resources/img.jpeg'))
+    browser.element('#currentAddress').should(be.blank).type('г. Магадан, ул. Ленина 21-72')
     browser.element('//*[contains(text(),"Select State")]').click().element('//*[contains(text(),"NCR")]').click()
     browser.element('//*[contains(text(),"Select City")]').click().element('//*[contains(text(),"Delhi")]').click()
 
-    browser.element('#submit').click()
+    browser.element('#submit').submit()
 
     browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
+    browser.element('.table').should(have.text('Ivan Khokhlov'
+                                               and 'ivan@example.com'
+                                               and '0123456789'
+                                               and '15 September,1993'
+                                               and 'г. Магадан, ул. Ленина 21-72'
+                                               and 'Maths'
+                                               and 'NCR'
+                                               and 'Delhi'))
 
 
 
